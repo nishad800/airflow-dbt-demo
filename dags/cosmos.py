2554e81b0d107c7c37fe7f964bd3fa58e1dc1fd0
nishad800 <145121025+nishad800@users.noreply.github.com>
2023-09-20 20:42:05 +0530

    Update cosmos.py

--- dags/cosmos.py
+++ dags/cosmos.py
@@ -1,15 +1,9 @@
 from cosmos import DbtTaskGroup, ProjectConfig, ProfileConfig
-from cosmos import SnowflakeUserPasswordProfileMapping
+
 
 # then, in your DAG
 jaffle_shop = DbtTaskGroup(
     project_config=ProjectConfig("/opt/airflow/git/airflow-dbt-demo.git"),
-    profile_config=ProfileConfig(
-        profile_name="demo_dbt",
-        target_name="dev",
-        profile_mapping=SnowflakeUserPasswordProfileMapping(
-            conn_id="dbt_demo",
-            profile_args={"schema": "public"},
-        ),
-    )
-)
+    conn_id="dbt_demo",
+            
+        )
