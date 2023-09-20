921927f13870c1ab428cfd17bebab716db254d8a
nishad800 <145121025+nishad800@users.noreply.github.com>
2023-09-20 18:58:17 +0530

    Update cosmos.py

--- dags/cosmos.py
+++ dags/cosmos.py
@@ -1,13 +1,22 @@
-from cosmos import DbtTaskGroup, ProjectConfig, ProfileConfig
+from cosmos import DbtTaskGroup, ProjectConfig, ProfileConfig, GitHubSourceConfig
 
-# then, in your DAG
+# Define the GitHub repository URL of your dbt project
+github_repo_url = "https://github.com/nishad800/airflow-dbt-demo.git"
+
+# Then, in your DAG
 jaffle_shop = DbtTaskGroup(
-    project_config=ProjectConfig(""),
+    project_config=ProjectConfig(
+        source_config=GitHubSourceConfig(
+            repo_url=github_repo_url,
+            # Optionally, specify a specific branch or commit
+            branch="main",  # Replace with your desired branch or commit
+        )
+    ),
     profile_config=ProfileConfig(
-        profile_name="my_profile",
-        target_name="my_target",
+        profile_name="profiles",
+        target_name="target",
         profile_mapping=PostgresUserPasswordProfileMapping(
-            conn_id="dbt_demo",
+            conn_id="demo_dbt",
             profile_args={"schema": "public"},
         ),
     )
